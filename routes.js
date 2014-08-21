e9b98541d8c7204ddf0ca664be317d60101dbff8
martzjd <martz.jd@gmail.com>
2014-08-21 15:42:02 -0500

    con el apoyo de

--- routes.js
+++ routes.js
@@ -28,7 +28,7 @@ app.post('/contact',function(req, res) {
 		if(!err) {
 			console.log('New user has been created');		
 			//res.redirect("https://s3.amazonaws.com/cannedhead.pillow/doc.pdf");
-			res.redirect('/'); 
+			res.redirect('/');
 
 		} else {
 			console.log('ERROR: ' + err);
