b638298cc6f9d899bc6631b4ec75ab757e36cf83
martzjd <martz.jd@gmail.com>
2014-08-29 16:55:51 -0500

    Landing update

--- routes.js
+++ routes.js
@@ -27,7 +27,7 @@ app.post('/contact',function(req, res) {
 	user.save(function(err) {
 		if(!err) {
 			console.log('New user has been created');		
-			//res.redirect("https://s3.amazonaws.com/cannedhead.pillow/doc.pdf");
+			res.redirect("https://db.tt/G2kFpAIr");
 			res.redirect('/');
 
 		} else {
